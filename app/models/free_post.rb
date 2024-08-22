6a3348d82373c10dc598dc81afed5a2980cfb0cb
fasfafal <nogulin0530@gmail.com>
2024-08-23 00:13:28 +0900

    issue交換用更新

--- app/models/free_post.rb
+++ app/models/free_post.rb
@@ -1,6 +1,6 @@
 class FreePost < ApplicationRecord
     validates :title, presence: true, length: { maximum: 255}
-    validates :body, presence: true, length: { maximum: 65_535}
+    validates :body, presence: true, length: { maximum: 10}
 
     belongs_to :user, optional: true
     has_many :comments
