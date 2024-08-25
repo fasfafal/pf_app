64020bd78c7dd49e24d1401bf4bc0f5efa6c450b
wassupdee <daichihirohata@DESKTOP-GDIBLU3>
2024-08-25 10:47:02 +0900

    自由投稿のtitleの文字数制限を255→100へ変更

--- app/models/free_post.rb
+++ app/models/free_post.rb
@@ -1,5 +1,5 @@
 class FreePost < ApplicationRecord
-    validates :title, presence: true, length: { maximum: 255}
+    validates :title, presence: true, length: { maximum: 100 }
     validates :body, presence: true, length: { maximum: 10}
 
     belongs_to :user, optional: true
