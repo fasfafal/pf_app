76b41527dd562d0981dbad894d14ece4f7a57b51
fasfafal <nogulin0530@gmail.com>
2024-09-06 22:58:19 +0900

    画像投稿の修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,9 +3,11 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
+  if Rails.env.production?
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = ENV['DIRECTORY'] # 作成したバケット名を記述
+    config.fog_public = false #ACLのエラー防止
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'], # 環境変数
@@ -13,5 +15,8 @@ CarrierWave.configure do |config|
       region: 'ap-northeast-3',   # アジアパシフィック(東京)を選択した場合
       path_style: true
     }
-    config.fog_public = false #ACLのエラー防止
+  else # 本番環境以外の場合はアプリケーション内にアップロード
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
+  end
 end
