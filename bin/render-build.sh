534759f22b64a19fe6278c3c4887de63523d8a2a
fasfafal <nogulin0530@gmail.com>
2024-10-09 14:02:47 +0900

    テスト

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,7 +4,7 @@ bundle install
 yarn install
 #yarn build jsファイルをesbuildでバンドルしているため
 bundle exec rails assets:clobber
-bundle exec rails assets:clean
 bundle exec rails assets:precompile
+bundle exec rails assets:clean
 bundle exec rails db:migrate
 #bundle exec rake db:migrate:reset
