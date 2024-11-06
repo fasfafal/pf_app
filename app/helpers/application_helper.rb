19e692a55ebbd929554be055543a5732499ae472
fasfafal <nogulin0530@gmail.com>
2024-11-06 15:51:01 +0900

    静的OGPの実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'ぶらっと',
+      title: 'ぶらっと',
+      reverse: true,
+      separator: '|',
+      description: 'ページの説明',
+      keywords: 'ページキーワード',
+      canonical: https://pf-app-udpx.onrender.com,
+      og: {
+        site_name: 'ぶらっと',
+        title: 'ぶらっと',
+        description: '献血を利用する人を増やす事を目的としたアプリです!',
+        type: 'website',
+        url: https://pf-app-udpx.onrender.com,
+        image: image_url('OGP.jpg'),
+        local: 'ja_JP',
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@https://x.com/5151b1236776',
+        image: image_url('OGP.jpg')
+      }
+    }
+  end
 end
