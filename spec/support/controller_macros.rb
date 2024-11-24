311833abd310c704834a921495dd6d646dcf85e7
fasfafal <nogulin0530@gmail.com>
2024-11-24 16:16:41 +0900

    lintチェックの修正

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -5,6 +5,7 @@ module ControllerMacros
       sign_in FactoryBot.create(:admin) # Using factory bot as an example
     end
   end
+
   def login_user
     before(:each) do
       @request.env["devise.mapping"] = Devise.mappings[:user]
