febd28236ee1aaa652f1665df82fc06f36c5b9a8
fasfafal <nogulin0530@gmail.com>
2024-12-23 22:26:20 +0900

    修正

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,6 @@
 class Reservation < ApplicationRecord
     belongs_to :user, optional: true
     validates :date, presence: true
-    validates :note, length: { maximum: 500 }
+    validates :note, length: { maximum: 400 }
   end
   
